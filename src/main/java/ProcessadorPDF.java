baa6396de5881fe6801e86e1c56033d4ec19294c
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-10-25 15:38:46 -0200

    refatoracao

--- src/main/java/ProcessadorPDF.java
+++ src/main/java/ProcessadorPDF.java
@@ -16,34 +16,15 @@ import java.util.ArrayList;
 import model.Materia;
  
 
-public class PDFRead {
+public class ProcessadorPDF {
+
+    public static void leitura_PDF(){
     
-  public static ArrayList isLinhaDeNota(String linha){
-      
-        ArrayList materias_cursadas = new ArrayList<>();
-       
-     if ( linha.contains("Aprovado") || linha.contains("Dispensa") || linha.contains("Reprovado")){ 
-         String [] frase = linha.split("\\s+");
-         for (int j = 0; j < frase.length; j++) {
-         String codigo = frase[0];
-         String situacao = frase[frase.length-1];
-         Materia materia = new Materia(codigo, situacao);
-         materias_cursadas.add(materia);
-//         System.out.println(codigo + situacao );
-         }
-     }
-        return materias_cursadas;
-  }
-    
-  public static void main(String args[]){
-    
-      try {
+        try {
 	//Create PdfReader instance.
 	PdfReader pdfReader = new PdfReader("file:///home/ricardo/Documentos/historicoEscolarCRAprovados.pdf");	
- 
 	//Get the number of pages in pdf.
 	int pages = pdfReader.getNumberOfPages(); 
- 
 	//Iterate the pdf through pages.
 	for(int i=1; i<=pages; i++) { 
 	  //Extract the page content using PdfTextExtractor.
@@ -51,19 +32,38 @@ public class PDFRead {
           String [] tokens = pageContent.split("\\n");
             for (int j = 0; j < tokens.length; j++) {
                 String token = tokens[j];
-                isLinhaDeNota(token); 
-                   
+                isLinhaDeNota(token);       
 //                System.out.println(token);
             }
-//	  System.out.println(var_1);
-      }
- 
-      //Close the PdfReader.
-      pdfReader.close();
-      } catch (Exception e) {
-	e.printStackTrace();
+        }
+        //Close the PdfReader.
+        pdfReader.close();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
     }
-  }
+    
+    public static ArrayList isLinhaDeNota(String linha){
+
+          ArrayList materias_cursadas = new ArrayList<>();
+
+       if ( linha.contains("Aprovado") || linha.contains("Dispensa") || linha.contains("Reprovado")){ 
+           String [] frase = linha.split("\\s+");
+           for (int j = 0; j < frase.length; j++) {
+           String codigo = frase[0];
+           String situacao = frase[frase.length-1];
+           Materia materia = new Materia(codigo, situacao);
+           materias_cursadas.add(materia);
+//           System.out.println(codigo + situacao );
+           }
+       }
+          return materias_cursadas;
+    }
+    
+    public static void main(String args[]){
+      leitura_PDF();
+
+    }
 }
 
  
