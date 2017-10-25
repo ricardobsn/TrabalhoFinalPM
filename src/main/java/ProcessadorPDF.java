8528be1fc58b25ed00384e2200f355a84e7bb783
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-10-25 16:38:47 -0200

    remocao repetidos wip

--- src/main/java/ProcessadorPDF.java
+++ src/main/java/ProcessadorPDF.java
@@ -17,6 +17,8 @@ import model.Materia;
  
 
 public class ProcessadorPDF {
+    
+    private static ArrayList<Materia> materias_cursadas = new ArrayList<Materia>();
 
     public static void leitura_PDF(){
     
@@ -45,8 +47,6 @@ public class ProcessadorPDF {
     
     public static ArrayList isLinhaDeNota(String linha){
 
-          ArrayList materias_cursadas = new ArrayList<>();
-
        if ( linha.contains("Aprovado") || linha.contains("Dispensa") || linha.contains("Reprovado")){ 
            String [] frase = linha.split("\\s+");
            for (int j = 0; j < frase.length; j++) {
@@ -60,8 +60,36 @@ public class ProcessadorPDF {
           return materias_cursadas;
     }
     
+   
+    // Faz um for percorrendo os itens da lista. Para cada item da lista percorrido, faz um outro for para percorrer todos os itens da lista novamente (Pulando o primeiro item do for externo)
+    // para verificar qual objeto tem id igual ao objeto do for mais externo e se são objetos diferentes.
+   
+    public static ArrayList remocaoMateriasRepetidas(ArrayList<Materia> materias){
+        
+        ArrayList<Materia> materias_cursadas_sem_repeticao = new ArrayList<Materia>();
+        for ( int i = 0; i < materias.size(); i++ ) {
+            for ( int j = i + 1; j < materias.size(); j++ ) {
+                // Se os objetos tiverem o mesmo id mas forem objetos diferentes, entao pegamos o campo primario do segundo objeto e o colocamos no campo secundario da primeiro objeto.
+                // Adicionamos o segundo objeto na lista para eliminação de objetos repetidos.
+                if ( ( materias.get(i).getCodigo()+materias.get(i).getSituacao()) == (materias.get(j).getCodigo()+materias.get(j).getSituacao()) &&
+                    !( ( (Object) materias.get(i) ).equals( materias.get(j) ) )  ) {
+
+                    i++; // Incrementa i em 1 para evitar verificações desnecessárias.
+                    break;
+                }
+            }
+        }
+        //elimina todos os elementos que foram marcados para remoção
+        materias.removeAll(materias_cursadas_sem_repeticao);
+        System.out.println(materias_cursadas_sem_repeticao);
+        
+       return materias_cursadas_sem_repeticao;
+    }
+    
+    
     public static void main(String args[]){
       leitura_PDF();
+//      remocaoMateriasRepetidas(materias_cursadas);
 
     }
 }
